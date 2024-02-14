14b47886b4fcb7d7ed327f54bbb9f30e7d6dac84
KustorAdam <kustoradam@gmail.com>
2024-02-14 23:44:36 +0100

    Update SideMenu.tsx

--- client/src/components/Dashboard/SideMenu.tsx
+++ client/src/components/Dashboard/SideMenu.tsx
@@ -12,6 +12,8 @@ import {
 } from "cdbreact";
 import { useCookies } from "next-client-cookies";
 import { FaBars } from "react-icons/fa";
+import Link from "next/link";
+import { LuLogOut } from "react-icons/lu";
 
 
 export default function SideMenu() {
@@ -23,6 +25,49 @@ export default function SideMenu() {
                 <CDBSidebarHeader prefix={<FaBars />}>
                     Kaja kezelő
                 </CDBSidebarHeader>
+                <CDBSidebarContent>
+                    <CDBSidebarMenu>
+                        <Link href="/">
+                            <CDBSidebarMenuItem>
+                                Főoldal
+                            </CDBSidebarMenuItem>
+                        </Link>
+                    </CDBSidebarMenu>
+                    <CDBSidebarMenu>
+                        <Link href="/menu">
+                            <CDBSidebarMenuItem>
+                                Heti Menü
+                            </CDBSidebarMenuItem>
+                        </Link>
+                    </CDBSidebarMenu>
+                    <CDBSidebarMenu>
+                        <Link href="/snake">
+                            <CDBSidebarMenuItem>
+                                Hosszú a sor?
+                            </CDBSidebarMenuItem>
+                        </Link>
+                    </CDBSidebarMenu>
+                    <CDBSidebarMenu>
+                        <Link href="/messages">
+                            <CDBSidebarMenuItem>
+                                Üzenetek
+                            </CDBSidebarMenuItem>
+                        </Link>
+                    </CDBSidebarMenu>
+                    <CDBSidebarMenu>
+                        <Link href="/user">
+                            <CDBSidebarMenuItem>
+                                Profil
+                            </CDBSidebarMenuItem>
+                        </Link>
+                    </CDBSidebarMenu>
+                </CDBSidebarContent>
+                <CDBSidebarFooter>
+                    <div className="d-flex align-items-center justify-content-center p-3" onClick={() => {t.remove("user"); location.href="/login"}} style={{"cursor": "pointer"}}>
+                        <LuLogOut className="me-2"/>
+                        <span>Kijelentkezés</span>
+                    </div>
+                </CDBSidebarFooter>
             </CDBSidebar>
         </div>
     )
