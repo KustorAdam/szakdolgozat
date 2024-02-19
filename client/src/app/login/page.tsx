a908744253dec002d68cd865ecd7e650b66d0b73
Kovács Zalán <145679545+KovacsZalan0603@users.noreply.github.com>
2024-02-19 22:46:09 +0100

    Add files via upload

--- client/src/app/login/page.tsx
+++ client/src/app/login/page.tsx
@@ -1,22 +1,52 @@
 
 
 import LoginForm from '@/components/login/LoginForm'
+import Link from 'next/link'
 import { Col, Row } from 'react-bootstrap'
 
 export default function login() {
 
   return (
-    <Row 
-      className='mt-10 pt-5 d-flex justify-content-center align-items-center'
-    >
-      <Col xs={10} md={5} lg={10} className='pb-4 pb-md-2'>
-        <h2>Bejelenetkezés</h2>
-      </Col>
-      <Col xs={10} md={5} lg={10}>
-        <LoginForm />
-      </Col>
-    </Row>
+    <div style={{
+      position: "absolute", top: ".5rem", right: ".5rem",
+      background: "gray",
+      border: "1px solid black",
+      padding: "1rem",
+      margin: "5rem",
+      borderRadius: ".5rem",
 
+
+
+    }}>
+      <Row
+        className='mt-10 pt-5 d-flex justify-content-center align-items-center'
+      >
+        <Col xs={10} md={5} lg={10} className='pb-4 pb-md-2'>
+          <h2>Jelentekezzen be!</h2>
+        </Col>
+
+
+        <Col xs={10} md={5} lg={10}>
+
+          <LoginForm />
+
+
+          <Col xs={10} md={5} lg={10} className='pb-4 pb-md-2'>
+
+          </Col>
+
+
+
+
+
+
+        </Col>
+        <Col xs={10} md={10} lg={10} className='pb-4 pb-md-2'>
+        
+          <h2>Még nincs fiókja? <Link href="/register">Regisztráljon</Link></h2>
+        </Col>
+      </Row>
+    </div>
 
   )
-  }
+}
