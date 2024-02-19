372dce4326c2c5b29c40583a9ae651798007375c
Kovács Zalán <145679545+KovacsZalan0603@users.noreply.github.com>
2024-02-19 22:46:37 +0100

    Add files via upload

---
+++ client/src/app/register/page.tsx
@@ -0,0 +1,15 @@
+import { Button, Form } from "react-bootstrap";
+
+export default function register() {
+    return (
+     
+        <h2>A regisztráció oldal valamiért egy zsák szaros gecis szar húgy</h2>
+    
+
+
+
+
+
+    )
+   
+}
