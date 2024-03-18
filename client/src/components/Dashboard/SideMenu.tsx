9db14cf45a4d006767928faa443f6e5d107a7899
KustorAdam <kustoradam@gmail.com>
2024-03-18 08:07:40 +0100

    Update SideMenu.tsx

--- client/src/components/Dashboard/SideMenu.tsx
+++ client/src/components/Dashboard/SideMenu.tsx
@@ -1,74 +1,23 @@
-'use client'
+'use client';
 
-import React from "react";
-import { 
-    CDBSidebar,
-    CDBSidebarHeader,
-    CDBSidebarContent,
-    CDBSidebarMenu,
-    CDBSidebarMenuItem,
-    CDBSidebarFooter,
-    CDBIcon
-} from "cdbreact";
+import { Sidebar, Menu, MenuItem } from "react-pro-sidebar";
 import { useCookies } from "next-client-cookies";
-import { FaBars } from "react-icons/fa";
-import Link from "next/link";
-import { LuLogOut } from "react-icons/lu";
-
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '@/styles/dashboard.css'
 
 export default function SideMenu() {
     const t = useCookies();
 
     return (
-        <div className="d-flex" style={{height: '100vh', overflow: 'scroll initial'}}>
-            <CDBSidebar textColor="#fff" backgroundColor="#333">
-                <CDBSidebarHeader prefix={<FaBars />}>
-                    Kaja kezelő
-                </CDBSidebarHeader>
-                <CDBSidebarContent>
-                    <CDBSidebarMenu>
-                        <Link href="/">
-                            <CDBSidebarMenuItem>
-                                Főoldal
-                            </CDBSidebarMenuItem>
-                        </Link>
-                    </CDBSidebarMenu>
-                    <CDBSidebarMenu>
-                        <Link href="/menu">
-                            <CDBSidebarMenuItem>
-                                Heti Menü
-                            </CDBSidebarMenuItem>
-                        </Link>
-                    </CDBSidebarMenu>
-                    <CDBSidebarMenu>
-                        <Link href="/snake">
-                            <CDBSidebarMenuItem>
-                                Hosszú a sor?
-                            </CDBSidebarMenuItem>
-                        </Link>
-                    </CDBSidebarMenu>
-                    <CDBSidebarMenu>
-                        <Link href="/messages">
-                            <CDBSidebarMenuItem>
-                                Üzenetek
-                            </CDBSidebarMenuItem>
-                        </Link>
-                    </CDBSidebarMenu>
-                    <CDBSidebarMenu>
-                        <Link href="/user">
-                            <CDBSidebarMenuItem>
-                                Profil
-                            </CDBSidebarMenuItem>
-                        </Link>
-                    </CDBSidebarMenu>
-                </CDBSidebarContent>
-                <CDBSidebarFooter>
-                    <div className="d-flex align-items-center justify-content-center p-3" onClick={() => {t.remove("user"); location.href="/login"}} style={{"cursor": "pointer"}}>
-                        <LuLogOut className="me-2"/>
-                        <span>Kijelentkezés</span>
-                    </div>
-                </CDBSidebarFooter>
-            </CDBSidebar>
-        </div>
+        <div className="d-flex" style={{height: 500, overflow: 'scroll initial'}}>
+            <Sidebar backgroundColor="8ecae6">
+                <Menu>
+                <MenuItem>Főoldal</MenuItem>
+                <MenuItem>Menü</MenuItem>
+                <MenuItem>Profil</MenuItem>
+                <MenuItem>Hosszú a sor?</MenuItem>
+                </Menu>
+             </Sidebar>
+       </div>
     )
 }
