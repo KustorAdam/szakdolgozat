f3583abe1f7602f79a6b514709db1d5b3e7589c9
KustorAdam <kustoradam@gmail.com>
2024-03-18 08:07:42 +0100

    Update page.tsx

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,8 +1,12 @@
 import { Container } from "react-bootstrap";
 import React from "react";
 
+const mystyle = {
+  padding: "10px",
+}
+
 export default function Home() {
   return (
-    <h2>Sidebar</h2>
+      <h2 style={mystyle}>Sidebar</h2>
   )
 }
