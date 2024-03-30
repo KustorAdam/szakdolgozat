60a888c487e7351ceff7f0e86bd8caa8368b5263
Kovács Zalán <145679545+KovacsZalan0603@users.noreply.github.com>
2024-03-30 22:14:34 +0100

    Add files via upload

---
+++ client/src/components/Nav/Nav.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "./nav.module.css";
+
+
+
+const Nav: React.FC<{}> = () => {
+    return(
+        <nav className={styles.navbar}>
+        <div className={styles['logo-container']}>
+            
+        </div>
+        </nav>
+    )
+
+    }
+export default Nav;
