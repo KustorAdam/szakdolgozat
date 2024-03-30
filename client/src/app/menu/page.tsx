15cf3b9b561b4019cc2752f3221af4789c41f01b
Kovács Zalán <145679545+KovacsZalan0603@users.noreply.github.com>
2024-03-30 22:08:41 +0100

    Add files via upload

--- client/src/app/menu/page.tsx
+++ client/src/app/menu/page.tsx
@@ -11,12 +11,14 @@ import { Form, Button, Container } from "react-bootstrap"
 
 export default function menu() {
     return (
+
+        
         <form>
             <div style={{
                 position: "absolute", top: "1rem", right: "", left: "17rem",
                 background: "#12427c",
                 border: "1px solid black",
-                padding: "11rem",
+                padding: "22rem",
                 margin: "1rem",
                 borderRadius: "5rem",
 
@@ -27,19 +29,29 @@ export default function menu() {
 
                 <Form.Group>
                     <Button style={{
-                            backgroundColor: "#5131de",
-                            borderColor: "#5131de"
+                        backgroundColor: "#5131de",
+                        borderColor: "#5131de",
+                        float: "right",
+                        position: "absolute",
+                        right: "70%",
+                        marginLeft: "91%",
+                        top: "20%",
+                        whiteSpace: "nowrap",
+                        textAlign: "center"
 
-                    }}      type="submit" variant="primary">Ez a hét</Button>
+                    }} type="submit" variant="primary">Ez a hét</Button>
                 </Form.Group>
                 <Form.Group>
-                    <Button   style={{
-                          backgroundColor: "#5131de",
-                          borderColor: "#5131de",
-                          float: "right",
-                          position: "absolute",
-                          right: "-91%",
-                          marginLeft: "91%"
+                    <Button style={{
+                        backgroundColor: "#5131de",
+                        borderColor: "#5131de",
+                        float: "right",
+                        position: "absolute",
+                        right: "20%",
+                        marginLeft: "91%",
+                        top: "20%",
+                        whiteSpace: "nowrap",
+                        textAlign: "center"
 
                     }} type="submit" variant="primary">Jövő hét</Button>
                 </Form.Group>
