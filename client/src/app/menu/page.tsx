cd0af1d08e5a05f72e2744ad2a1944164bb70d54
Kovács Zalán <145679545+KovacsZalan0603@users.noreply.github.com>
2024-04-11 02:25:01 +0200

    Add files via upload

--- client/src/app/menu/page.tsx
+++ client/src/app/menu/page.tsx
@@ -2,73 +2,135 @@
 'use client';
 
 import { useCookies } from "next-client-cookies";
-
 import React from "react";
-import { Form, Button, Container } from "react-bootstrap"
+import { Form, Button, Container, Card } from "react-bootstrap"
 
 
 
 
 export default function menu() {
     return (
-
-        
         <form>
-            <div style={{
-                position: "absolute", top: "1rem", right: "", left: "17rem",
+
+
+            <Form.Group>
+                <Button style={{
+                     boxShadow: "2px 2px 5px",
+                    backgroundColor: "#5131de",
+                    borderColor: "#5131de",
+                    float: "right",
+                    position: "absolute",
+                    borderRadius: "7px",
+                    right: "75%",
+                    marginLeft: "91%",
+                    top: "20%",
+                    whiteSpace: "nowrap",
+                    textAlign: "center",
+                    width: "80px",
+                    height: "60px"
+
+                }} type="submit" variant="primary">Ez a hét</Button>
+            </Form.Group>
+            <Form.Group>
+                <Button style={{
+                     boxShadow: "2px 2px 5px",
+                    backgroundColor: "#5131de",
+                    borderColor: "#5131de",
+                    float: "right",
+                    position: "absolute",
+                    borderRadius: "7px",
+                    marginLeft: "27%",
+                    top: "20%",
+                    whiteSpace: "nowrap",
+                    textAlign: "center",
+                    width: "80px",
+                    height: "60px"
+                }} type="submit" variant="primary">Jövő hét</Button>
+            </Form.Group>
+
+
+            <Card style={{
+ boxShadow: "2px 2px 5px",
+                position: "absolute", top: "4rem", right: "", left: " 7rem",
                 background: "#12427c",
                 border: "1px solid black",
-                padding: "22rem",
-                margin: "1rem",
-                borderRadius: "5rem",
+                padding: "1rem",
+                margin: "11rem",
+                borderRadius: "2rem",
+                width: "180px",
+                height: "300px"
+            }}></Card>
+            <Card style={{
+ boxShadow: "2px 2px 5px",
+                position: "absolute", top: "4rem", right: "", left: " 22rem",
+                background: "#12427c",
+                border: "1px solid black",
+                padding: "1rem",
+                margin: "11rem",
+                borderRadius: "2rem",
+                width: "180px",
+                height: "300px"
+            }}></Card>
+
+            <Card style={{
+ boxShadow: "2px 2px 5px",
+                position: "absolute", top: "4rem", right: "", left: " 37rem",
+                background: "#12427c",
+                border: "1px solid black",
+                padding: "1rem",
+                margin: "11rem",
+                borderRadius: "2rem",
+                width: "180px",
+                height: "300px"
+            }}></Card>
+
+            <Card style={{
+  boxShadow: "2px 2px 5px",
+                position: "absolute", top: "4rem", right: "", left: " 52rem",
+                background: "#12427c",
+                border: "1px solid black",
+                padding: "1rem",
+                margin: "11rem",
+                borderRadius: "2rem",
+                width: "180px",
+                height: "300px"
+            }}></Card>
+
+            <Card style={{
+  boxShadow: "2px 2px 5px",
+                position: "absolute", top: "4rem", right: "", left: " 67rem",
+                background: "#12427c",
+                border: "1px solid black",
+                padding: "1rem",
+                margin: "11rem",
+                borderRadius: "2rem",
+                width: "180px",
+                height: "300px"
+              
+            }}></Card>
+
+<Form.Group>
+                <Button style={{
+                    backgroundColor: "#5131de",
+                    borderColor: "#5131de",
+                    float: "right",
+                    position: "absolute",
+                    borderRadius: "7px",
+                    marginLeft: "88%",
+                    top: "81%",
+                    whiteSpace: "nowrap",
+                    textAlign: "center",
+                    width: "120px",
+                    height: "60px",
+                    fontSize: "18px",
+                    boxShadow: "2px 2px 5px"
+                }} type="submit" variant="primary">Mentés</Button>
+            </Form.Group>
 
 
 
-
-            }}>
-
-                <Form.Group>
-                    <Button style={{
-                        backgroundColor: "#5131de",
-                        borderColor: "#5131de",
-                        float: "right",
-                        position: "absolute",
-                        right: "70%",
-                        marginLeft: "91%",
-                        top: "20%",
-                        whiteSpace: "nowrap",
-                        textAlign: "center"
-
-                    }} type="submit" variant="primary">Ez a hét</Button>
-                </Form.Group>
-                <Form.Group>
-                    <Button style={{
-                        backgroundColor: "#5131de",
-                        borderColor: "#5131de",
-                        float: "right",
-                        position: "absolute",
-                        right: "20%",
-                        marginLeft: "91%",
-                        top: "20%",
-                        whiteSpace: "nowrap",
-                        textAlign: "center"
-
-                    }} type="submit" variant="primary">Jövő hét</Button>
-                </Form.Group>
-
-
-
-
-
-
-
-
-            </div>
 
         </form>
-
-
-
     )
 
 }
