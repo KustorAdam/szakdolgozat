d4504f3e289ee03c189cd3b9fdd8a2af87258f9b
KustorAdam <kustoradam@gmail.com>
2024-04-17 08:19:09 +0200

    Create RegistrationResponse.cs

---
+++ KEK_API/APIModels/Models/RegistrationResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIModels.Models
+{
+    public class RegistrationResponse
+    {
+        public bool IsRegSuccess { get; set; } = false;
+    }
+}
