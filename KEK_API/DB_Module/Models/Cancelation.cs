acb939eed769927971db15f7555ac86fc6cb6d76
KustorAdam <kustoradam@gmail.com>
2024-04-17 17:51:28 +0200

    Update Cancelation.cs

--- KEK_API/DB_Module/Models/Cancelation.cs
+++ KEK_API/DB_Module/Models/Cancelation.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace DB_Module.Models
 {
-    [Table("lemondasok")]
+    [Table("Cancelation")]
     public class Cancelation
     {
         [Required][Key] public int Id { get; set; }
