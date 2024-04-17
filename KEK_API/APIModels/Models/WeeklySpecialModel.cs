2e345ea641cdfbcd62d329091aa0a508b7d22dee
KustorAdam <kustoradam@gmail.com>
2024-04-17 21:53:59 +0200

    Create WeeklySpecialModel.cs

---
+++ KEK_API/APIModels/Models/WeeklySpecialModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIModels.Models
+{
+    internal class WeeklySpecialModel
+    {
+        public int Id { get; set; }
+        public string Breakfast { get; set; }
+        public string Lunch { get; set; }
+        public string Dinner { get; set; }
+    }
+}
