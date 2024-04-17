86f03d691124f2c26b2173345c4ace6680afda87
KustorAdam <kustoradam@gmail.com>
2024-04-17 08:08:05 +0200

    Create Messages.cs

---
+++ KEK_API/DB_Module/Models/Messages.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DB_Module.Models
+{
+    [Table("messages")]
+    public class Messages
+    {
+        [Required]int Id { get; set; }
+        [Required] string Message { get; set; }
+        [Required] DateTime DateTime { get; set; }
+    }
+}
